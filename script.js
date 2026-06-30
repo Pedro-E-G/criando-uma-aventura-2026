3f89776ba0910115d90612068aa08d540271c9b4
Pedro-E-G <pedro.paulino.garcia@escola.pr.gov.br>
2026-06-30 13:36:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+script.js
+Página
+1
+/
+1
+100%
+const avanca = document.querySelectorAll('.btn-proximo');
 
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
+Exibindo script.js…
